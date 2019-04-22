ae41f9550611118b684165b6c2b4b79073171be0
Samuel T <ssamt1142@gmail.com>
2019-04-22 00:17:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require_relative "../lib/concerns/memorable"
+require_relative "../lib/concerns/findable"
 
 class Song
   attr_accessor :name
@@ -12,9 +13,7 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  
 
   def self.all
     @@songs
