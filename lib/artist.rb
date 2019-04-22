40633583d8420496bd1e039ffafdd6d2e0c6a520
Samuel T <ssamt1142@gmail.com>
2019-04-22 00:56:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,14 @@
 require 'pry'
 require_relative "../lib/concerns/memorable"
 require_relative "../lib/concerns/findable"
+require_relative "../lib/concerns/paramble"
 
 class Artist
   attr_accessor :name
   attr_reader :songs
   extend Memorable
   extend Findable
+  include Paramble
 
   @@artists = []
 
